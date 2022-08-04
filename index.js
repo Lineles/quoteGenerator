875337eaeed1d6459d94e651b3d617d863d71610
Lineles <106559385+Lineles@users.noreply.github.com>
2022-08-04 09:41:09 +0200

    addet justify content function

--- index.js
+++ index.js
@@ -2,16 +2,11 @@
                 
 
 
-// create function color change
-        // this function should take 1 parameter (witch is either the choosen collor or a random collor)    
+// Color Change    
 function colorChange(input) {
         const collorArray = ["#cf2626", "#db9e2c", "#c0c00b", "#0e910e", "#11dfdf", "#2f32e9", "#63159b", "#8f178f", "#e27487"];
-        const color = collorArray[input]     
-             
-// it should change the Backgroundcollor of the main Div element to the chosen color
-        document.getElementById("quote-background-flex").style.backgroundColor = color;
-        
-// it should also make the border of the aktiv button red      
+        const color = collorArray[input]             
+        document.getElementById("quote-background-flex").style.backgroundColor = color;         
 }
        
 
@@ -20,23 +15,31 @@ function colorChange(input) {
         // this function should give you the posebiliti to choose all 3 options (bold/italic/underlined) at the same time
         // it should also randomly select the options if the random button is presst
         // it should change the Quotes text style to the chosen one
-        // it should also make the border of the aktiv button red
+        
 
 
 
-// create function text Alighnt change
+// create function text Justify change
+function justifyText(input) {
+        const alighments = ["flex-start", "center", "flex-end"]
+        const alighment = alighments[input];
+        document.getElementById("quote-background-flex").style.justifyContent = alighment;
+}
         // this function should take 1 parameter (witch is either the choosen alighment or a random alighment)
         // it should cange the Quote alighment to the chosen one
-        // it should also make the border of the aktiv button red
+        
 
 
 
 // create function quote change 
         // this function should randomly change the showen Quote
         // it should also get triggert when the random button ist presst
-        //
+        
 
 
 // create function random everything 
         // this function should 
-                // randomly chose a collor and call the color function with 
+                // randomly chose a collor and call the color function with it 
+                // randomly chose a text Style and call the text style function
+                // randomly chose a quote
+                // randomly chose a alighment
